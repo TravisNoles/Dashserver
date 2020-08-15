1b3b3b049c2bbf9c756f7b9856a1e0211f054042
travis <larupia@gmail.com>
2020-08-15 16:29:56 -0500

    Edited newgameserver form appearance

--- gamemgr/forms.py
+++ gamemgr/forms.py
@@ -2,13 +2,13 @@ from django import forms
 
 
 class GameServerForm(forms.Form):
-    name = forms.CharField()
+    name = forms.CharField(label='Game Server Name: ', max_length=50)
+
     type = forms.ChoiceField(choices=[
         ('terraria', 'Terraria'),
         ('minecraft', 'Minecraft')
         ])
 
 
-    gameserver = forms.CharField(label='Game Server', max_length=20)
 
 
