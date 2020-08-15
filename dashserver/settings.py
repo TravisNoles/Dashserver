aa3569ede5453918800d3dfbb835d2c8bc71e9c2
travis <larupia@gmail.com>
2020-08-15 10:50:46 -0500

    Fixed security key leak with python-dotenv library

--- dashserver/settings.py
+++ dashserver/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
-import os
+import os, dotenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,7 +21,15 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 's4ew7klbud8qs%ic*n!nbsu024oo@*zgmu%klb-+#h27r8hj5s'
+
+# Update secret key to get from .env file inside dashserver root directory.
+# Set secret key inside base directory (where manage.py is located)
+
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
+
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
